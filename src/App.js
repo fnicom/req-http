0baa1c3a3d0cb7593329a769d1d052ca1d686967
fnicom <fnicomedes@gmail.com>
2022-09-05 18:58:14 -0300

    adjust returno app

--- src/App.js
+++ src/App.js
@@ -3,11 +3,11 @@ import "./App.css";
 import { useState, useEffect } from "react";
 
 function App() {
-  (
+  return (
     <>
-    <h1>Requisiçao http</h1>
+      <h1>Requisiçao http</h1>
     </>
-  )
+  );
 }
 
 export default App;
