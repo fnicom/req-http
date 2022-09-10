4b94031b50e59e4eb388e85a97127f62004562ae
fnicom <fnicomedes@gmail.com>
2022-09-10 15:54:35 -0300

    fetch dados

--- src/App.js
+++ src/App.js
@@ -2,11 +2,37 @@ import "./App.css";
 
 import { useState, useEffect } from "react";
 
+const url = "http://localhost:3000/products";
+
 function App() {
+  const [products, setProducts] = useState([]);
+
+  // Resgatando dados
+  useEffect(() => {
+    async function fetchData() {
+      const res = await fetch(url);
+
+      const data = await res.json();
+
+      setProducts(data);
+
+      console.log(products);
+    }
+
+    fetchData();
+  }, []);
+
   return (
-    <>
-      <h1>Requisiçao http</h1>
-    </>
+    <div className="App">
+      <h1>Lista de produtos</h1>
+      <ul>
+        {products.map((product) => (
+          <li key={product.id}>
+            {product.name} - R$: {product.price}
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 }
 
